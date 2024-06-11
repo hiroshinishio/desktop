18195c4561529b957cc1ac52b70fd743be774ad2
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2024-06-11 11:38:32 +0200

    fix compilation by exporting a newly needed function
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/libsync/clientsideencryption.h
+++ src/libsync/clientsideencryption.h
@@ -121,7 +121,7 @@ private:
 
 namespace EncryptionHelper {
 
-QByteArray generateRandomFilename();
+OWNCLOUDSYNC_EXPORT QByteArray generateRandomFilename();
 OWNCLOUDSYNC_EXPORT QByteArray generateRandom(int size);
 QByteArray generatePassword(const QString &wordlist, const QByteArray& salt);
 OWNCLOUDSYNC_EXPORT QByteArray encryptPrivateKey(
